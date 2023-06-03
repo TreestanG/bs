86c475aed3e666dc662fa2040935ccbd697c0b88
Koolwiza <tristangee@gmail.com>
2023-06-03 11:18:49 -0700

    changed block creation

--- index.py
+++ index.py
@@ -1,14 +1,5 @@
-import pygame
-import sched
-import time
-import random
+import pygame, time, random
 from pygame.locals import *
-
-pygame.init()
-window_width = 800
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Tetris - lol")
 
 BLACK = (0, 0, 0)
 CYAN = (0, 255, 255)
@@ -32,70 +23,46 @@ shape_colors = {
 block_size = 30
 shape_size = 4
 
+tetris_grid = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16],
+]
 
 shapes = {
-    "I": [
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ],
-    "O": [
-        [1, 1, 0, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ],
-    "T": [
-        [0, 1, 0, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ],
-    "L": [
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0]
-    ],
-    "J": [
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0]
-    ],
-    "S": [
-        [0, 1, 1, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ],
-    "Z": [
-        [1, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
-    ]
+    "I":[[5,6,7,8], [3,7,11,15], [9,10,11,12], [2,6,10,14]],
+    "O":[[2,3,6,7], [2,3,6,7], [2,3,6,7], [2,3,6,7]],
+    "T":[[2,5,6,7], [2,6,7,10], [5,6,7,10], [2,5,6,10]],
+    "L":[[2,6,10,11], [5,6,7,11], [1,2,6,10], [2,6,7,8]],
+    "J":[[2,6,9,10], [1,5,6,7], [2,3,6,10], [5,6,7,11]],
+    "S":[[2,3,5,6], [2,6,7,11], [2,3,5,6], [2,6,7,11]],
+    "Z":[[1,2,6,7], [3,6,7,10], [1,2,6,7], [3,6,7,10]]
 }
 
 
+
 class TetrisBlock:
-    def __init__(self, x, y, shape, block = None):
+    def __init__(self, x, y, shape):
         self.x = x
         self.y = y
         self.shape = shape
         self.color = shape_colors[shape]
         self.rotation = 0
-        self.block = block
+        self.code = shapes[shape][self.rotation]
 
     def rotate_cw(self):
-        self.block = pygame.transform.rotate(self.block, 90)
-    
+        rotation += 1
+        if rotation > 3:
+            rotation = 0
+
     def rotate_ccw(self):
-        self.block = pygame.transform.rotate(self.block, -90)
+        rotation -= 1
+        if rotation < 0:
+            rotation = 3
 
     def move_down(self):
-        if self.check_collision():
+        #if self.check_collision():
             self.y += 1
 
     def check_collision(self):
@@ -110,11 +77,20 @@ class TetrisBlock:
 
     def move_right(self):
         self.x += 1
+    
+    def update(self, game_running):
+        print(game_running)
+        while game_running == True:
+            time.sleep(1.25)
+            self.move_down()
 
-    def update_board(self):
-        block_scheduler = sched.scheduler(time.time, time.sleep)
-        block_scheduler.enter(1.25, 1, self.move_down, (block_scheduler,))
-        block_scheduler.run()
+
+
+pygame.init()
+window_width = 800
+window_height = 600
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Tetris - lol")
 
 
 class Tetris:
@@ -123,27 +99,29 @@ class Tetris:
         self.board_height = 20
         self.block_shape = 30
         self.shape_size = 4
-        self.board = [[0 for x in range(self.board_width)] for y in range(self.board_height)]
+        self.board = [[0 for x in range(self.board_width)]
+                      for y in range(self.board_height)]
         self.current_block = TetrisBlock(5, 1, self.shape())
 
     def draw_block(self, x, y, color):
         pygame.draw.rect(window, color, (x, y, block_size, block_size))
         pygame.draw.rect(window, (41, 44, 53), (x, y, block_size, block_size), 1)
-        return
 
     def draw_shape(self, x, y, shape, color):
-        for row in range(shape_size):
-            for col in range(shape_size):
-                if shape[row][col]:
-                    self.draw_block(x + col * block_size, y + row * block_size, color)
+        for row in range(len(tetris_grid)):
+            for e in range(len(tetris_grid[row])):
+                if tetris_grid[row][e] in self.current_block.code:
+                    self.draw_block(x+(e)*block_size, y+(row-1)*block_size, color)                    
 
     def draw_board(self):
         window.fill(BLACK)
         windowDimensions = {"x": block_size*10, "y": block_size*20}
-        pygame.draw.rect(window, (41, 44, 53), (250, 0, windowDimensions["x"], windowDimensions["y"]))
+        pygame.draw.rect(window, (41, 44, 53), (250, 0,
+                         windowDimensions["x"], windowDimensions["y"]))
 
         for i in range(250, 550, block_size):
-            pygame.draw.line(window, (75, 75, 75), (i, 0), (i, windowDimensions["y"]), 1)
+            pygame.draw.line(window, (75, 75, 75), (i, 0),
+                             (i, windowDimensions["y"]), 1)
         for y in range(0, 600, block_size):
             pygame.draw.line(window, (75, 75, 75), (250, y), (550, y), 1)
 
@@ -154,53 +132,54 @@ class Tetris:
             return (number)*block_size
 
     def shape(self):
-        tetris_block = self.generate_block()
-        self.current_block = tetris_block
-
         return random.choice(list(shapes.keys()))
 
     def generate_block(self):
+
         x = self.board_width // 2
         y = 0
         shape = self.shape()
-        return TetrisBlock(x, y, shape)
-    
-    def handle_input(self):
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return True
-            elif event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                    self.current_block.move_left()
-                elif event.key == K_RIGHT:
-                    self.current_block.move_right()
-                elif event.key == K_UP:
-                    self.current_block.rotate_cw()
-                elif event.key == K_DOWN:
-                    self.current_block.rotate_ccw()
 
+        tetris_block = TetrisBlock(x, y, shape)
+        self.current_block = tetris_block
+
+        return tetris_block
+
+    def handle_input(self, event):
+        if event.type == QUIT:
+            return True
+        elif event.type == KEYDOWN:
+            print("key down")
+            if event.key == K_LEFT:
+                self.current_block.move_left()
+            elif event.key == K_RIGHT:
+                self.current_block.move_right()
+            elif event.key == K_UP:
+                self.current_block.rotate_cw()
+            elif event.key == K_DOWN:
+                print("down key pressed")
+                self.current_block.rotate_ccw()
 
     def run(self):
         running = True
+        self.draw_board()
+
+        self.current_block.block = self.draw_shape(
+            self.tetris_coords("x", self.current_block.x),  # 340
+            self.tetris_coords("y", self.current_block.y),  # 30
+            self.current_block.shape,
+            self.current_block.color
+        )
 
         while running:
-            self.handle_input()
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
-
-            self.draw_board()
-            self.current_block.block = self.draw_shape(
-                self.tetris_coords("x", self.current_block.x),
-                self.tetris_coords("y", self.current_block.y),
-                shapes[self.current_block.shape],
-                self.current_block.color
-            )
-
-            time.sleep(1.25)
-            self.current_block.move_down()
+                
+                self.handle_input(event)
 
             pygame.display.update()
+
         pygame.quit()
 
 
