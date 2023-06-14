5b11bd059a629be1dc7ea3d6801745f5b4e6af12
Koolwiza <108306482+TreestanG@users.noreply.github.com>
2023-06-14 23:38:21 +0800

    added boundaries to side to side, finishing up collisions

--- index.py
+++ index.py
@@ -16,6 +16,7 @@ clock = pygame.time.Clock()
 class Tetris:
     def __init__(self):
         self.current_block = TetrisBlock(5, 1, self.shape())
+        self.move_start_time = 0
 
     def draw_board(self):
         window.fill(BLACK)
@@ -34,41 +35,71 @@ class Tetris:
             return 250 + (number-2)*block_size
         elif label == "y":
             return (number)*block_size
+        
+    def draw_past_blocks(self):
+        for row in range(len(tetris_matrix)):
+            for col in range(len(tetris_matrix[row])):
+                if tetris_matrix[row][col] != 0:
+                    pygame.draw.rect(window, bricks[tetris_matrix[row][col]], (self.tetris_coords("x", col+2),
+                                     self.tetris_coords("y", row), block_size, block_size))
+
 
     def shape(self):
-        return random.choice(list(shapes.keys()))
+        bag = []
+        for shape in range(len(shapes.keys())):
+            bag.append(random.choice(list(shapes.keys())))
+            bag.append(random.choice(list(shapes.keys())))
+        
+        if len(bag) > 0:
+            return bag.pop(random.choice(range(len(bag))))
+        else:
+            for shape in range(len(shapes.keys())):
+                bag.append(random.choice(list(shapes.keys())))
+                bag.append(random.choice(list(shapes.keys())))
+            return bag.pop(random.choice(range(len(bag))))
 
-    def handle_input(self, event, k_down_active):
+    def handle_input(self, event):
 
         if event.type == KEYDOWN:
-            if event.key == K_DOWN and not k_down_active >= 300:
-                self.current_block.move_down()
-                k_down_active = True
-            if event.key == K_LEFT:
-                self.current_block.move_left()
-            if event.key == K_RIGHT:
-                self.current_block.move_right()
+            self.move_start_time = pygame.time.get_ticks()            
+        elif event.type == KEYUP:
+            if self.move_start_time != 0 and pygame.time.get_ticks() - self.move_start_time >= 300:
+                if event.key == K_DOWN:
+                    if not self.current_block.check_collision("v"):
+                        self.current_block.move_down()
+                    self.move_start_time = pygame.time.get_ticks()
+                if event.key == K_LEFT:
+                    if not self.current_block.check_collision("l"):
+                        self.current_block.move_left()
+                    self.move_start_time = pygame.time.get_ticks()
+                if event.key == K_RIGHT:
+                    if not self.current_block.check_collision("r"):
+                        self.current_block.move_right()
+                    self.move_start_time = pygame.time.get_ticks()
+            else:
+                if event.key == K_DOWN:
+                    self.current_block.move_down()
+                if event.key == K_LEFT:
+                    self.current_block.move_left()
+                if event.key == K_RIGHT:
+                    self.current_block.move_right()
             if event.key == K_UP:
                 self.current_block.rotate_cw()
-        elif event.type == KEYUP:
-            if event.key == K_DOWN and k_down_active >= 300:
-                k_down_active = 0
+            
 
     def run(self):
         running = True
-        k_down_tracker = 0
 
         while running:
-            k_down_tracker += 30
-           # print(k_down_tracker)
 
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
-                self.handle_input(event, k_down_tracker)
+                self.handle_input(event)
 
             window.fill((0, 0, 0))
             self.draw_board()
+            self.draw_past_blocks()
             self.current_block.draw_shape(
                 self.tetris_coords("x", self.current_block.x),  # 340
                 self.tetris_coords("y", self.current_block.y),  # 30
--- tetris_classes.py
+++ tetris_classes.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 from definitions import *
 
 class tetris_piece:
@@ -19,6 +19,7 @@ class TetrisBlock:
         self.start_time = pygame.time.get_ticks()
         self.new_block = False
         self.reached_bottom = False
+        self.block_coords = []
 
     def rotate_cw(self):
         self.rotation += 1
@@ -32,43 +33,68 @@ class TetrisBlock:
             self.rotation = 3
         self.code = shapes[self.shape][self.rotation]
 
-    def move_down(self):
-        print(self.check_collision(), self.reached_bottom)
-        if not self.check_collision() or not self.reached_bottom:
-            print('isnt')
-            self.y += 1
-        elif self.reached_bottom: 
-            print('elif')
-            self.new_block = True
-
-    def check_collision(self):
+    def check_collision(self, direction):
         check_left = dimensions[self.shape][0][self.rotation]+2
         check_right = 10 - dimensions[self.shape][1][self.rotation]
-        if self.y >= 19:
-            self.y = 19
-            self.reached_bottm = True
-            return True
-        elif self.x <= check_left:
-            return True
-        elif self.x >= check_right:
-            return True
+        print(self.x, self.y)
+        if direction == "v":
+            if self.y >= 19:
+                self.y = 19
+                self.reached_bottom = True
+
+                return True
+        elif direction == "r":
+            if self.x >= check_right:
+                return True
+        elif direction == "l":
+            if self.x <= check_left:
+                return True
         return False
+    
+    def block_collision(self):
+        blk_dimensions = shape_dimensions[self.shape]
+        if self.rotation == 1 or self.rotation == 3:
+            blk_dimensions = blk_dimensions.reverse()
+
+        available_spaces = []
+        
+        for row in range(len(tetris_matrix)):
+            for col in range(len(tetris_matrix[row])):
+                if tetris_matrix[row][col] == 0:
+                    available_spaces.append((row, col))
+        for coords in self.block_coords:
+            if coords not in available_spaces:
+                if self.y <= 0:
+                    return True   
+        return False
+    
+    def move_down(self):
+        if not self.check_collision("v") or not self.reached_bottom:
+            if self.block_collision():
+                self.reached_bottom = True
+            self.y += 1
+        elif self.reached_bottom: 
+            self.new_block = True
 
     def move_left(self):
-        if (self.check_collision() and not self.reached_bottom) or not self.check_collision():
+        if not self.check_collision("l"):
             self.x -= 1
 
     def move_right(self):
-        if (self.check_collision() and not self.reached_bottom) or not self.check_collision:
+        if not self.check_collision("r"):
             self.x += 1
 
     def draw_block(self, x, y, color, window):
         return pygame.draw.rect(window, color, tetris_piece(x,y))
 
     def draw_shape(self, x, y, color, window):
+        self.block_coords = []
         for row in range(len(tetris_grid)):
             for e in range(len(tetris_grid[row])):
                 if tetris_grid[row][e] in self.code:
+                    coord = (math.floor((x+(e)*block_size)/block_size)-6, math.floor((y+(row-1)*block_size)/block_size)+1)
+                    if coord not in self.block_coords:
+                        self.block_coords.append(coord)
                     (self.draw_block(x+(e)*block_size, y+(row-1)*block_size, color, window))
     
     def update(self):
